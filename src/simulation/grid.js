38c871240718db24907915285eded41f91833f65
Roland Siegbert <roland.siegbert@telekom.de>
2020-04-25 12:56:27 +0200

    Match EU countries inside the centroids

--- src/simulation/grid.js
+++ src/simulation/grid.js
@@ -20,5 +20,20 @@ export function applyFixedNodeGrid(nodes, height=600) {
     node.fy = fy;
   }
 
+  fetch('https://raw.githubusercontent.com/eusim/coronavirus-simulation/master/data/country_centroids_az8.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data));
+
+    for (i = 0; i < data.features.length; i++){
+      if (data.features[i].properties.region_un == 'Europe' &&
+        (data.features[i].properties.type == 'Country' || true)) {
+        console.log(data.features[i].properties.admin)
+      }
+    }
+  });
+
   return nodes;
 }
