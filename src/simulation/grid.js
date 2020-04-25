7c0afce71a12fce05b82ab632193bd7e83a04b00
Roland Siegbert <roland.siegbert@telekom.de>
2020-04-26 01:05:23 +0200

    Add European flags to each and every country in the map
    Even Kosovo.

--- src/simulation/grid.js
+++ src/simulation/grid.js
@@ -1,33 +1,40 @@
 import { shuffle } from '../utils';
 
-export function applyFixedNodeGrid(nodes, height=600) {
+export function applyFixedNodeGrid(nodes, height = 600) {
   shuffle(nodes);
 
   const gridSize = 100;
   const nodesToAlign = nodes.filter(({ type }) => type === 'venue');
   const count = nodesToAlign.length;
-  
+
   fetch('https://raw.githubusercontent.com/eusim/coronavirus-simulation/master/data/country_centroids_az8.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(JSON.stringify(data));
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(JSON.stringify(data));
 
-    var venue = 0;
-    for (var i = 0; (i < data.features.length && venue < count); i++){
-      if (data.features[i].properties.region_un === 'Europe' &&
-        (data.features[i].properties.type === 'Country' || data.features[i].properties.type === 'Sovereign country')) {
-        console.log(data.features[i].properties.admin);
-        const node = nodesToAlign[venue];
+      var venue = 0;
+      for (var i = 0; (i < data.features.length && venue < count); i++) {
+        if (data.features[i].properties.region_un === 'Europe'
+          && (data.features[i].properties.type === 'Country'
+          || data.features[i].properties.type === 'Sovereign country')
+          && data.features[i].properties.admin !== 'Russia') {
+          const node = nodesToAlign[venue];
 
-        node.fx = data.features[i].geometry.coordinates[0]*25;
-        node.fy = data.features[i].geometry.coordinates[1]*7;
+          node.fx = data.features[i].geometry.coordinates[0] * 14 + 300;
+          node.fy = - data.features[i].geometry.coordinates[1] * 14 + 1100;
 
-        venue++;
+          // TODO: Kosovo, we feel your pain
+          if (data.features[i].properties.iso_a2 == -99) {
+            data.features[i].properties.iso_a2 = "XK";
+          }
+          node.venue = data.features[i].properties.iso_a2;
+
+          venue++;
+        }
       }
-    }
-  });
+    });
 
   return nodes;
 }
